f51ac36f0ca50606ed042ce7958fe1cd69ba739f
p4p1 <p4p1@vivaldi.net>
2022-10-17 00:53:14 +0100

    added html page to setup token

--- dist/index.js
+++ dist/index.js
@@ -9691,7 +9691,52 @@ const github = __nccwpck_require__(5438);
 const LINKEDIN_SECRET = core.getInput("LINKEDIN_SECRET");
 core.setSecret(LINKEDIN_SECRET);
 
-console.log("helloWorld");
+function sendPostLinkedIn(title, url, desc) {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${process.env.LINKEDIN_SECRET}`);
+  myHeaders.append("Content-Type", "application/json");
+  var raw = JSON.stringify({
+    "author": "urn:li:person:s2fNfmiGu2",
+    "lifecycleState": "PUBLISHED",
+    "specificContent": {
+      "com.linkedin.ugc.ShareContent": {
+        "shareCommentary": {
+          "text": `${desc}`
+        },
+        "shareMediaCategory": "ARTICLE",
+        "media": [
+          {
+            "status": "READY",
+            "description": {
+              "text": `${title}`
+            },
+            "originalUrl": `${url}`,
+            "title": {
+              "text": `${title}`
+            }
+          }
+        ]
+      }
+    },
+    "visibility": {
+      "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+    }
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.linkedin.com/v2/ugcPosts", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
+sendPostLinkedIn("example", "https://leosmith.xyz", "A quick example to test some stuff out")
 
 })();
 
